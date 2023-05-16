720369d1e1431191254c7c2da086fa418d73110f
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2023-05-15 22:23:20 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'test') {
+        interaction.reply('test');
+    }
+});
+
+client.login(process.env.TOKEN);
